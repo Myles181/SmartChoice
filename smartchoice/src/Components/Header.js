52d8f311a9cc967a37d527cb75a02a19727b6a60
Isaac Olowookere <olowookere.isaaco@gmail.com>
2023-07-20 05:29:04 +0100

    continued the alli express

--- smartchoice/src/Components/Header.js
+++ smartchoice/src/Components/Header.js
@@ -1,86 +1,109 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 import "../Styles/Header.css";
-import SearchIcon from '@mui/icons-material/Search';
-import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
-import MenuIcon from '@mui/icons-material/Menu';
-import { Button, Divider, IconButton, MenuItem, Toolbar, Typography } from '@mui/material';
+import SearchIcon from "@mui/icons-material/Search";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  Button,
+  Divider,
+  IconButton,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 //import { Script } from 'vm';
 
-
-
 function Header() {
-    const [input, setInput] = useState("");    
-    const menuOption = ['Home', 'MarketPlace', 'Trending-products', 'Newsletter'];
+  const [input, setInput] = useState("");
+  const menuOption = ["Home", "MarketPlace", "Trending-products", "Newsletter"];
 
-    function handleSearch(e) {
-        e.preventDefault();
-        setInput("");
+  function handleSearch(e) {
+    e.preventDefault();
+    setInput("");
 
-        console.log({input});
-        //console.log({'message': searchData});
-    }
+    fetch("http://localhost:5200/product", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(input),
+      mode: "cors", // Set the mode to 'cors' to enable CORS
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        console.log("successful");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
 
-    return (
-        <div className='header'>
-            <div className='header__width'>
-                <div className='header__content'>
-                    <div className='header__contentUp'>
-                        <div className='header__logo'>
-                            <img src='\Images\Main Logologo.png' alt='Smart Choice logo'/>
-                            <div className='header__search'>
-                                {/* <div className='header__input'>
+    // console.log({input});
+    //console.log({'message': searchData});
+  }
+
+  return (
+    <div className="header">
+      <div className="header__width">
+        <div className="header__content">
+          <div className="header__contentUp">
+            <div className="header__logo">
+              <img src="\Images\Main Logologo.png" alt="Smart Choice logo" />
+              <div className="header__search">
+                {/* <div className='header__input'>
                                     <SearchIcon />
                                     <input type='text' />
                                 </div> */}
-                                <div className='header__menu'>
-                                    <Toolbar>
-                                        <IconButton>
-                                            <MenuIcon />
-                                        </IconButton>
-                                        <Typography>
-                                            Categories
-                                        </Typography>
-                                    </Toolbar>
-                                        <Divider orientation='vertical' variant='middle' flexItem/>
-                                    {menuOption.map((menu) => (
-                                        <MenuItem key={menu}>
-                                            <Typography textAlign='center'>{menu}</Typography>
-                                        </MenuItem>
-                                    ))}
-                                </div>
-                                
-                            </div>
-                        </div>
-                        <div className='header__input'>
-                            <SearchIcon />
-                            <input type='text' id='searchInput' value={input} onChange={ e => setInput(e.target.value)} placeholder='Search products, brands and Categories' />
-                        </div>
-                        <div className='header__searchBtn'>
-                            <Button variant="contained" size="small" onClick={handleSearch}>Search</Button>
-                        </div>
-                        <div className='header__help'>
-                            <Toolbar>
-                                <IconButton>
-                                    <HelpOutlineIcon />
-                                </IconButton>
-                                <Typography>
-                                    Help
-                                </Typography>
-                                <div className='header__contact'>
-                                    <Typography className='header__contactUs'>Contact us</Typography>
-                                    <Typography>
-                                        About
-                                    </Typography>
-                                </div>
-                            </Toolbar>
-                        </div>
-                    </div>
+                <div className="header__menu">
+                  <Toolbar>
+                    <IconButton>
+                      <MenuIcon />
+                    </IconButton>
+                    <Typography>Categories</Typography>
+                  </Toolbar>
+                  <Divider orientation="vertical" variant="middle" flexItem />
+                  {menuOption.map((menu) => (
+                    <MenuItem key={menu}>
+                      <Typography textAlign="center">{menu}</Typography>
+                    </MenuItem>
+                  ))}
                 </div>
+              </div>
             </div>
+            <div className="header__input">
+              <SearchIcon />
+              <input
+                type="text"
+                id="searchInput"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                placeholder="Search products, brands and Categories"
+              />
+            </div>
+            <div className="header__searchBtn">
+              <Button variant="contained" size="small" onClick={handleSearch}>
+                Search
+              </Button>
+            </div>
+            <div className="header__help">
+              <Toolbar>
+                <IconButton>
+                  <HelpOutlineIcon />
+                </IconButton>
+                <Typography>Help</Typography>
+                <div className="header__contact">
+                  <Typography className="header__contactUs">
+                    Contact us
+                  </Typography>
+                  <Typography>About</Typography>
+                </div>
+              </Toolbar>
+            </div>
+          </div>
         </div>
-    )
-    
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
