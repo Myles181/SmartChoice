17c84791997edfe39b784a9106368266c83c2ab3
Isaac Olowookere <olowookere.isaaco@gmail.com>
2023-07-21 19:18:59 +0100

    resolved merge conflicts

--- smartchoice/src/Components/TrendingCard.js
+++ smartchoice/src/Components/TrendingCard.js
@@ -3,49 +3,56 @@ import "../Styles/TrendingCard.css";
 import { Typography } from "@mui/material";
 
 function TrendingCard() {
-
-    return (
-        <div className='trendCard'>
-            <div className='trendCard__box'>
-                <div className='trendCard__compare'>
-                    <h5>Compare Now</h5>
-                </div>
-                <div className='trendCard__img'>
-                    <img src='Images\childhood-cute-pair-child-baby.jpg' alt='Baby Shoes' />
-                </div>
-                <div className='trendCard__name'>
-                    <Typography className='trendCard__textHead' variant='h6' align='left' color='text.primary'>
-                        iPhone
-                    </Typography>
-                    <Typography className='trendCard__text' variant='body2' color="text.secondary">
-                        Gold Color with 256GB and a pineapple phone cover
-                    </Typography>
-                </div>
-            </div>
-
+  return (
+    <div>
+      <div className="trendCard">
+        <div className="trendCard__box">
+          <div className="trendCard__compare">
+            <h5>Compare Now</h5>
+          </div>
+          <div className="trendCard__img">
+            <img
+              src="Images\childhood-cute-pair-child-baby.jpg"
+              alt="Baby Shoes"
+            />
+          </div>
+          <div className="trendCard__name">
+            <Typography
+              className="trendCard__textHead"
+              variant="h6"
+              align="left"
+              color="text.primary"
+            >
+              iPhone
+            </Typography>
+            <Typography
+              className="trendCard__text"
+              variant="body2"
+              color="text.secondary"
+            >
+              Gold Color with 256GB and a pineapple phone cover
+            </Typography>
+          </div>
         </div>
-        <div className="trendCard__img">
-          <img
-            src="Images\childhood-cute-pair-child-baby.jpg"
-            alt="Baby Shoes"
-          />
-        </div>
-        <div className="trendCard__name">
-          <Typography
-            className="trendCard__text"
-            variant="h6"
-            color="text.primary"
-          >
-            iPhone
-          </Typography>
-          <Typography
-            className="trendCard__text"
-            variant="body2"
-            color="text.secondary"
-          >
-            Gold Color with 256GB and a pineapple phone cover
-          </Typography>
-        </div>
+      </div>
+      <div className="trendCard__img">
+        <img src="Images\childhood-cute-pair-child-baby.jpg" alt="Baby Shoes" />
+      </div>
+      <div className="trendCard__name">
+        <Typography
+          className="trendCard__text"
+          variant="h6"
+          color="text.primary"
+        >
+          iPhone
+        </Typography>
+        <Typography
+          className="trendCard__text"
+          variant="body2"
+          color="text.secondary"
+        >
+          Gold Color with 256GB and a pineapple phone cover
+        </Typography>
       </div>
     </div>
   );
